604d98e75222230048e46248d3897b1546d6d34a
Eugene Petrovich <petrovich.eugene@gmail.com>
2015-10-06 16:00:17 +0600

    Added: empty standard MainWindow functions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,13 +1,69 @@
 //==========================================================
 #include "MainWindow.h"
 #include "glVariables.h"
+
+#include <QMessageBox>
+#include <QCloseEvent>
+#include <QAction>
 //==========================================================
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    zh_createActions();
+    zh_createComponents();
+    zh_createMenu();
+    zh_createToolbar();
+    zh_createConnections();
+    zh_restoreSettings();
 }
 //==========================================================
 MainWindow::~MainWindow()
+{
+
+}
+//==========================================================
+void MainWindow::closeEvent(QCloseEvent* e)
+{
+    QString questionString = tr("Application will be closed. Are you sure?");
+    if(QMessageBox::question(this, tr("Application exit"), questionString,
+                             QMessageBox::Yes, QMessageBox::No) == QMessageBox::No)
+    {
+        e->ignore();
+        return;
+    }
+}
+//==========================================================
+void MainWindow::zh_createActions()
+{
+
+}
+//==========================================================
+void MainWindow::zh_createComponents()
+{
+
+}
+//==========================================================
+void MainWindow::zh_createMenu()
+{
+
+}
+//==========================================================
+void MainWindow::zh_createToolbar()
+{
+
+}
+//==========================================================
+void MainWindow::zh_createConnections()
+{
+
+}
+//==========================================================
+void MainWindow::zh_restoreSettings()
+{
+
+}
+//==========================================================
+void MainWindow::zh_saveSettings()
 {
 
 }
--- mainwindow.h
+++ mainwindow.h
@@ -11,6 +11,22 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+
+private:
+    // VARS
+
+
+    // FUNCS
+    void closeEvent(QCloseEvent* e);
+    void zh_createActions();
+    void zh_createComponents();
+    void zh_createMenu();
+    void zh_createToolbar();
+    void zh_createConnections();
+    void zh_restoreSettings();
+    void zh_saveSettings();
+
 };
 //==========================================================
 #endif // MAINWINDOW_H
